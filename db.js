c1ee28d89906fe27685c6927bfbb08619463f285
Kevin Jordan <macdev2@Kevins-MacBook-Pro.local>
2019-12-23 13:08:45 -0800

    script fix

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mongodb = require('mongodb');
 const dotenv = require('dotenv').config();
 
-let port = process.env.PORT || 8080;
+// let port = process.env.PORT || 8080;
 
-if(port == null || port == "") {
-    port = 3001
-}
+// if(port == null || port == "") {
+//     port = 3001
+// }
 
 mongodb.connect(process.env.CONNECTIONSTRING, {useUnifiedTopology: true, useNewUrlParser: true}, function(err, client) {
     module.exports = client;
